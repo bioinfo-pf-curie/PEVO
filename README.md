a818b83dec928c0e3f9139fdf0c8a2fd1e98e9a0
nservant <nicolas.servant@curie.fr>
2025-04-29 13:39:42 +0200

    [MODIF] release codes

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # PEVO
-Code used for the analysis of the PEVO project
+
+Code used for the analysis of the PEVO project.
+
+Borcoman E\*, Cabarrou B\*, Francisco M\*, et al. Efficacy of pembrolizumab and vorinostat combination in patients with recurrent and/or metastatic squamous cell carcinomas: a phase II basket trial.
+
+### Requirements
+
+The following R packages must be installed to use the scripts :
+- tidyverse
+- ggplot2
+- ComplexHeatmap
+
+
--- scripts/code_for_figure5.r
+++ scripts/code_for_figure5.r
@@ -1,5 +1,5 @@
 # ---
-#   title: "NATCANCER_code Figure 4"
+#   title: "NATCANCER_code_for_figure_5"
 # author: "Miguel Francisco"
 # date: "24/03/2025"
 # ---
@@ -11,8 +11,8 @@ library(ComplexHeatmap)
 require(RColorBrewer)
 
 #data prep
-test <- read.csv("pathto/oncoprint_baseline_mat_for_NatCancer.csv")
-p_data_ordered <- read.csv("pathto/p_data_for_NatCancer.csv") 
+test_2 <- read.csv("../data/oncoprint_pathways_baseline_mat_for_NatCancer.csv")
+p_data_ordered <- read.csv("../data/p_data_for_NatCancer.csv") 
 
 #colors
 col_anno <- c("Anus" = "lemonchiffon1", 
@@ -47,33 +47,51 @@ col_anno <- c("Anus" = "lemonchiffon1",
               "Tobacco" = "tomato",
               "UV" = "royalblue1")
 
-## Figure 4
+## Figure 5
 #sample split
-mat_for_onco_baseline_ORR_res <- test[,colnames(test) %in% (subset(p_data_ordered, `CR_PR` == "Yes", select = sample_id_clinsight) %>% pull())]
-mat_for_onco_baseline_ORR_nonres <- test[,colnames(test) %in% (subset(p_data_ordered, `CR_PR` == "No", select = sample_id_clinsight) %>% pull())]
+mat_for_onco_baseline_pathways_ORR_res <- test_2[,colnames(test_2) %in% (subset(p_data_ordered, `CR_PR` == "Yes", select = sample_id_clinsight) %>% pull())]
+mat_for_onco_baseline_pathways_ORR_nonres <- test_2[,colnames(test_2) %in% (subset(p_data_ordered, `CR_PR` == "No", select = sample_id_clinsight) %>% pull())]
 
 #preparation for annotations
 p_data_ordered_ORR_res <- p_data_ordered[p_data_ordered$sample_id_clinsight %in% (subset(p_data_ordered, `CR_PR` == "Yes", select = sample_id_clinsight) %>% pull()),]
 p_data_ordered_ORR_nonres <- p_data_ordered[p_data_ordered$sample_id_clinsight %in% (subset(p_data_ordered, `CR_PR` == "No", select = sample_id_clinsight) %>% pull()),]
 
 #column order
-onco.id <- match(p_data_ordered_ORR_res$sample_id_clinsight, colnames(mat_for_onco_baseline_ORR_res))
-onco_colnames_ORR_res <- colnames(mat_for_onco_baseline_ORR_res)[onco.id]
-mat_for_onco_baseline_ORR_res_test <- mat_for_onco_baseline_ORR_res[onco.id]
+onco.id <- match(p_data_ordered_ORR_res$sample_id_clinsight, colnames(mat_for_onco_baseline_pathways_ORR_res))
+onco_colnames_ORR_res <- colnames(mat_for_onco_baseline_pathways_ORR_res)[onco.id]
+mat_for_onco_baseline_pathways_ORR_res_test <- mat_for_onco_baseline_pathways_ORR_res[onco.id]
 
-onco.id <- match(p_data_ordered_ORR_nonres$sample_id_clinsight, colnames(mat_for_onco_baseline_ORR_nonres))
-onco_colnames_ORR_nonres <- colnames(mat_for_onco_baseline_ORR_nonres)[onco.id]
-mat_for_onco_baseline_ORR_nonres_test <- mat_for_onco_baseline_ORR_nonres[onco.id]
+onco.id <- match(p_data_ordered_ORR_nonres$sample_id_clinsight, colnames(mat_for_onco_baseline_pathways_ORR_nonres))
+onco_colnames_ORR_nonres <- colnames(mat_for_onco_baseline_pathways_ORR_nonres)[onco.id]
+mat_for_onco_baseline_pathways_ORR_nonres_test <- mat_for_onco_baseline_pathways_ORR_nonres[onco.id]
 
 #annotations
+p_data_test_anno <- HeatmapAnnotation(`Primary cancer site` = p_data_ordered_ORR$primary_cancer_site,
+                                      TMB = p_data_ordered_ORR$`TMB (high and low)`,
+                                      MSI = p_data_ordered_ORR$`MSI/MSS`,
+                                      AS = p_data_ordered_ORR$AS_bi,
+                                      HPV = p_data_ordered_ORR$`HPV (positive/negative)`,
+                                      `CR/PR` = p_data_ordered_ORR$CR_PR,
+                                      `PD-L1` = p_data_ordered_ORR$`PD-L1 IHC: positive/negative`,
+                                      `Major mutational signature` = p_data_ordered_ORR$Short_names,
+                                      col = list(`Primary cancer site` = col_anno,
+                                                 HPV = col_anno,
+                                                 MSI = col_anno,
+                                                 AS = col_anno,
+                                                 TMB = col_anno,
+                                                 `PD-L1` = col_anno,
+                                                 `CR/PR` = col_anno,
+                                                 `Major mutational signature` = col_anno))
+
 p_data_test_anno_ORR_res <- HeatmapAnnotation(`Primary cancer site` = p_data_ordered_ORR_res$primary_cancer_site,
                                               TMB = p_data_ordered_ORR_res$`TMB (high and low)`,
                                               MSI = p_data_ordered_ORR_res$`MSI/MSS`,
                                               AS = p_data_ordered_ORR_res$AS_bi,
                                               HPV = p_data_ordered_ORR_res$`HPV (positive/negative)`,
-                                              `CR/PR` = p_data_ordered_ORR_res$`CR_PR`,
+                                              `CR/PR` = p_data_ordered_ORR_res$CR_PR,
                                               `PD-L1` = p_data_ordered_ORR_res$`PD-L1 IHC: positive/negative`,
                                               `Major mutational signature` = p_data_ordered_ORR_res$Short_names,
+                                              show_annotation_name = TRUE,
                                               col = list(`Primary cancer site` = col_anno,
                                                          HPV = col_anno,
                                                          MSI = col_anno,
@@ -81,8 +99,7 @@ p_data_test_anno_ORR_res <- HeatmapAnnotation(`Primary cancer site` = p_data_ord
                                                          TMB = col_anno,
                                                          `PD-L1` = col_anno,
                                                          `CR/PR` = col_anno,
-                                                         `Major mutational signature` = col_anno),
-                                              show_annotation_name = TRUE)
+                                                         `Major mutational signature` = col_anno))
 
 p_data_test_anno_ORR_nonres <- HeatmapAnnotation(`Primary cancer site` = p_data_ordered_ORR_nonres$primary_cancer_site,
                                                  TMB = p_data_ordered_ORR_nonres$`TMB (high and low)`,
@@ -92,6 +109,7 @@ p_data_test_anno_ORR_nonres <- HeatmapAnnotation(`Primary cancer site` = p_data_
                                                  `CR/PR` = p_data_ordered_ORR_nonres$CR_PR,
                                                  `PD-L1` = p_data_ordered_ORR_nonres$`PD-L1 IHC: positive/negative`,
                                                  `Major mutational signature` = p_data_ordered_ORR_nonres$Short_names,
+                                                 show_annotation_name = FALSE,
                                                  col = list(`Primary cancer site` = col_anno,
                                                             HPV = col_anno,
                                                             MSI = col_anno,
@@ -99,22 +117,21 @@ p_data_test_anno_ORR_nonres <- HeatmapAnnotation(`Primary cancer site` = p_data_
                                                             TMB = col_anno,
                                                             `PD-L1` = col_anno,
                                                             `CR/PR` = col_anno,
-                                                            `Major mutational signature` = col_anno),
-                                                 show_annotation_name = FALSE)
+                                                            `Major mutational signature` = col_anno))
 
-#oncoprint figure 4
-oncoprint <- oncoPrint(mat_for_onco_baseline_ORR_nonres_test,
-                       alter_fun = alter_fun, 
-                       col = col.oncoprint,
-                       show_column_names = FALSE,
-                       remove_empty_columns = FALSE,
-                       show_row_names = FALSE,
-                       column_order = onco_colnames_ORR_nonres,
-                       bottom_annotation = p_data_test_anno_ORR_nonres,
-                       column_title = "Patients not experiencing an objective response",
-                       row_split = oncogene[match(rownames(mat_for_onco_baseline_ORR_nonres_test), oncogene$Hugo.Symbol),"class"],
-                       show_heatmap_legend = FALSE) +
-  oncoPrint(mat_for_onco_baseline_ORR_res_test,
+#oncoprint for Figure 5
+oncoprint_pathway <- oncoPrint(mat_for_onco_baseline_pathways_ORR_nonres_test,
+                               alter_fun = alter_fun, 
+                               col = col.oncoprint,
+                               show_column_names = FALSE,
+                               remove_empty_columns = FALSE,
+                               show_row_names = FALSE,
+                               column_order = onco_colnames_ORR_nonres,
+                               bottom_annotation = p_data_test_anno_ORR_nonres,
+                               top_annotation = NULL,
+                               column_title = "Patients not experiencing an objective response",
+                               show_heatmap_legend = FALSE) +
+  oncoPrint(mat_for_onco_baseline_pathways_ORR_res_test,
             alter_fun = alter_fun, 
             col = col.oncoprint,
             show_column_names = FALSE,
@@ -122,8 +139,8 @@ oncoprint <- oncoPrint(mat_for_onco_baseline_ORR_nonres_test,
             remove_empty_columns = FALSE,
             column_order = onco_colnames_ORR_res,
             bottom_annotation = p_data_test_anno_ORR_res,
+            top_annotation = NULL,
             column_title = "Patients experiencing an objective response",
-            row_split = oncogene[match(rownames(mat_for_onco_baseline_ORR_res_test), oncogene$Hugo.Symbol),"class"],
-            show_heatmap_legend = TRUE)
+            show_heatmap_legend = FALSE)
 
-draw(oncoprint)
+oncoprint_pathway
